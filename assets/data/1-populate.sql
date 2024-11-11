420d7bfb9ec3ad55f30154f11a6d9a2f2fd4cdaa
lucastamura <lucas10.tamura@gmail.com>
2024-11-11 18:02:19 +0000

    trabalho final

--- assets/data/1-populate.sql
+++ assets/data/1-populate.sql
@@ -1,3 +1,5 @@
 USE application;
 
-INSERT INTO users(username, password) VALUES ('admin', 'root');
+INSERT INTO usuarios(nome_de_usuario, senha) VALUES (
+    'admin',
+    '$2a$12$EEctVN0iTsvALwgMvJsbfuYk0gjTEpttdRNeqL9vTOlAOhL4nfGRe');
